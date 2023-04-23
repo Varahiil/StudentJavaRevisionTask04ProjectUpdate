57fc349bcf78d0641ba64fc6d0e84b2a383889d6
Varahiil <Uriilpro33@gmail.com>
2023-04-23 14:11:15 +0300

    Task 04. Ordering Numbers [упорядочивание чисел]. Well Done. 2-а решения.

--- src/by/itstep/javatraining/revision/task/Task04.java
+++ src/by/itstep/javatraining/revision/task/Task04.java
@@ -23,6 +23,51 @@ package by.itstep.javatraining.revision.task;
 
 public class Task04 {
     public static String task04(int a, int b, int c) {
-        return "";
+        int min = a;
+        int middle = b;
+        int max = c;
+        String result = "";
+
+//        if (a >= b) {
+//            if (a >= c) {
+//                max = a;
+//                min = c;
+//                if (c >= b) {
+//                    middle = c;
+//                    min = b;
+//                }
+//            } else {
+//                middle = a;
+//                min = b;
+//            }
+//        } else if (a >= c) {
+//            middle = a;
+//            max = b;
+//            min = c;
+//        } else if (b >= c) {
+//            middle = c;
+//            max = b;
+//        }
+
+        if (a >= b && a >= c) {
+            max = a;
+        } else if (b >= c) {
+            max = b;
+        }
+
+        if (b <= a && b <= c) {
+            min = b;
+        } else if (c < a) {
+            min = c;
+        }
+
+        if (min == b && max == a || min == a && max == b) {
+            middle = c;
+        } else if (min == c && max == b || min == b && max == c) {
+            middle = a;
+        }
+
+        result = min + " " + middle + " " + max;
+        return result;
     }
 }
